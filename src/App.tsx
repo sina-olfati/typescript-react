e0b67d0bc2a9d60c019b576d80fa140e15920f72
sina <sinaolfati5@gmail.com>
2022-06-19 15:20:21 +0430

    Making a component (Example.tsx)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Example from './Example';
+
+function App() {
+  return (
+    <div className="App">
+     Sina
+     <Example />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Example.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+    name?: string,
+    age?: number,
+    fn?: (name: string) => void
+}
+
+const Example: React.FC<Props> = () => {
+  return (
+    <div>
+      <input name = "Sina" />
+    </div>
+  )
+}
+
+export default Example

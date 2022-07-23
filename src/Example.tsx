fce712580533b4aea7582fe9791beacd0b667365
sina <sinaolfati5@gmail.com>
2022-07-23 19:49:55 +0430

    useReducer

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,14 +1,34 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef, useEffect, useReducer} from 'react'
 
 interface Props {
     name: string,
     age?: number
 }
 
+export interface Name {
+  one: number;
+  two: string;
+}
+
+type Actions =
+  | {type: "add"; payload: string}
+  | {type: "remove"; payload: number};
+
+  const nameReducer = (state: Name[], action: Actions) => {
+    switch (action.type) {
+      case "add":
+        return state
+      case "remove":
+        return state
+    }
+  }
+
 const Example: React.FC<Props> = ({}) => {
   const [count, setCount] = useState<number>(0);
 
   const refs = useRef<HTMLInputElement>(null);
+
+  const [state, dispatch] = useReducer(nameReducer, [])
 
   return (
     <div>
